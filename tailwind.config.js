2175b4e20079c9832f0cb296919b591aef9fb709
hoseon07 <a01081930429@gmail.com>
2024-08-11 18:24:58 +0900

    기본 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+//tailwind.config.js
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extends: {},
+    fontSize: {
+      title1: 60,
+      title2: 36,
+      title3: 30,
+      body1: 25,
+      body2: 20,
+      body3: 16,
+      tiny: 12
+    },
+    fontFamily: {
+      pretendard: ['Pretendard']
+    }
+  }
+}
